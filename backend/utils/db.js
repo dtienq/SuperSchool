9e6ab66dcdd8347ebd78891641d12be314aed473
1660612 <1660612@student.hcmus.edu.vn>
2021-01-14 02:16:15 +0700

    db

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -4,11 +4,12 @@ const knex = require('knex')({
   client: 'pg',
   searchPath: 'superschool',
   connection: {
-    host: 'localhost',
-    user: 'postgres',
-    password: '123456',
-    database: 'SuperSchool',
-    port: 5432
+    host: 'ec2-107-23-191-123.compute-1.amazonaws.com',
+    user: 'wugnkgddbhxpda',
+    password: 'd39ab45b9583e403b1ba2b57bdccab5340fef202167f113183f9c91708237432',
+    database: 'd292tnhhqe64v7',
+    port: 5432,
+    ssl: true
   },
   pool: {
     min: 0,
