3cf173d2c94091ef93e4517e23b98b1946e4c29b
PhanThanh07 <phanthanhvi97@gmail.com>
2021-01-13 22:49:53 +0700

    Done topbar

---
+++ frontend/src/components/TopBar/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function index() {
+  return (
+    <div className="topbar-one">
+      <div className="container">
+        <div className="topbar-one__left">
+          <Link>phanthanhvi97@gmail.com</Link>
+          <Link>0983345305</Link>
+        </div>
+        <div className="topbar-one__right">
+          <Link to={'/login'}>Đăng nhập</Link>
+          <Link to={'/register'}>Đăng ký</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default index;
