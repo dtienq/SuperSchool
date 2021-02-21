5e436390ac93d2319c2bc0753b3b822ec9bac1ef
PhanThanh07 <phanthanhvi97@gmail.com>
2021-02-22 02:39:28 +0700

    Done authen BE

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,15 +1,22 @@
-const userModel = require('../models/user.model');
-var express = require('express');
+const userModel = require("../models/user.model");
+var express = require("express");
 var router = express.Router();
-const validation = require('../middlewares/validate.mdw');
-const loginSchema = require('../schemas/login.json');
-const registerSchema = require('../schemas/register.json');
-const jwt = require('jsonwebtoken');
-const commonUtils = require('../utils/common');
-const constant = require('../utils/constant');
-const bcrypt = require('bcrypt');
-const randomstring = require('randomstring');
-const db = require('../utils/db');
+const { OAuth2Client } = require("google-auth-library");
+const validation = require("../middlewares/validate.mdw");
+const loginSchema = require("../schemas/login.json");
+const registerSchema = require("../schemas/register.json");
+const jwt = require("jsonwebtoken");
+const commonUtils = require("../utils/common");
+const constant = require("../utils/constant");
+const bcrypt = require("bcrypt");
+const randomstring = require("randomstring");
+const db = require("../utils/db");
+// const { json } = require("body-parser");
+const omit = require("lodash.omit");
+const refreshTokenSchema = require("../schemas/refresh-token.json");
+const { request } = require("express");
+const mailService = require("../utils/mailService");
+const rn = require("random-number");
 
 /**
  * @api {post} /api/auth/login Đăng nhập
@@ -29,35 +36,42 @@ const db = require('../utils/db');
  *         "refresh_token": "QhnibXASPJRiDt46hDBWQGljURlZnfgkTOV8U6FRCkVqo0P3Rwx4QBjl9DBFyqLYt41jEiKQWjhUCTFn0ESFu2HLGgiu8pAXolHvhCGihEdCBKne3rh4erT5vv3Kc3yjM4EvjQ4Czine7D15oKT5Qh4d5uKgTrHao3BlzceL7HuTL9WKcWMn9YR9OVDoyjGsRwvNXKqE85xaT4XMWtoIdyH2vGCWTiVVF5T4cV2N2Ju4l7bVNrgD5CyXBd4gUFl"
  *     }
  */
-router.post('/login', validation(loginSchema), function (req, res, next) {
-    let userInfo = req.body;
+router.post("/login", validation(loginSchema), function (req, res, next) {
+  let userInfo = req.body;
 
-    userModel.getByUserName(userInfo.username).then(data => {
-        if (data) {
-            let isValid = bcrypt.compareSync(userInfo.password, data.password);
-            //check password
-            if (isValid) {
-                data.password = undefined;
-                const access_token = jwt.sign(commonUtils.parse2Plain(data), constant.SECRET_KEY);
-                const refresh_token = data.refresh_token;
-                data.refresh_token = undefined;
+  userModel
+    .getByUserEmail(userInfo.email)
+    .then((data) => {
+      if (data) {
+        let isValid = bcrypt.compareSync(userInfo.password, data.password);
+        //check password
+        if (isValid) {
+          data.password = undefined;
+          const access_token = jwt.sign(
+            commonUtils.parse2Plain(data),
+            constant.SECRET_KEY
+          );
+          const refresh_token = data.refresh_token;
+          data.refresh_token = undefined;
 
-                res.json({
-                    access_token: access_token,
-                    refresh_token: refresh_token,
-                    user: data
-                });
-            } else {
-                res.status(401).json({
-                    message: 'Username or password are not correct'
-                });
-            }
+          res.json({
+            status: true,
+            access_token: access_token,
+            refresh_token: refresh_token,
+            user: data,
+          });
         } else {
-            res.status(401).json({
-                message: 'Username or password are not correct'
-            });
+          res.status(401).json({
+            message: "Email or password are not correct",
+          });
         }
-    }).catch(next);
+      } else {
+        res.status(401).json({
+          message: "Email or password are not correct",
+        });
+      }
+    })
+    .catch(next);
 });
 
 /**
@@ -80,29 +94,230 @@ router.post('/login', validation(loginSchema), function (req, res, next) {
  *         "refresh_token": "QhnibXASPJRiDt46hDBWQGljURlZnfgkTOV8U6FRCkVqo0P3Rwx4QBjl9DBFyqLYt41jEiKQWjhUCTFn0ESFu2HLGgiu8pAXolHvhCGihEdCBKne3rh4erT5vv3Kc3yjM4EvjQ4Czine7D15oKT5Qh4d5uKgTrHao3BlzceL7HuTL9WKcWMn9YR9OVDoyjGsRwvNXKqE85xaT4XMWtoIdyH2vGCWTiVVF5T4cV2N2Ju4l7bVNrgD5CyXBd4gUFl"
  *     }
  */
-router.post('/register', validation(registerSchema), function (req, res, next) {
-    let userInfo = req.body;
+router.post("/register", function (req, res, next) {
+  let userInfo = req.body;
+  console.log(req.body);
+  let newUser = {
+    username: userInfo.email,
+    password: userInfo.password,
+    usergroupid: 2,
+    fullname: userInfo.username,
+    email: userInfo.email,
+  };
+  newUser.refresh_token = randomstring.generate({ length: 255 });
+  newUser.password = bcrypt.hashSync(newUser.password, constant.SALT_ROUNDS);
 
-    userInfo.refresh_token = randomstring.generate({ length: 255 });
-    userInfo.password = bcrypt.hashSync(userInfo.password, constant.SALT_ROUNDS);
-
-    db.transaction(transaction => {
-        userModel.create(transaction, userInfo).then(data => {
-            let access_token = '';
-
-            data.refresh_token = undefined;
-            access_token = jwt.sign(commonUtils.parse2Plain(data), constant.SECRET_KEY);
-
-            transaction.commit();
-            res.json({
-                refresh_token: userInfo.refresh_token,
-                access_token: access_token
-            });
-        }).catch(err => {
-            transaction.rollback();
-            next(err);
+  db.transaction((transaction) => {
+    userModel
+      .create(transaction, newUser)
+      .then((data) => {
+        let access_token = "";
+        data.refresh_token = undefined;
+        access_token = jwt.sign(
+          commonUtils.parse2Plain(data),
+          constant.SECRET_KEY
+        );
+        transaction.commit();
+        res.json({
+          status: true,
+          refresh_token: newUser.refresh_token,
+          access_token: access_token,
         });
+      })
+      .catch((err) => {
+        transaction.rollback();
+        next(err);
+      });
+  });
+});
+
+//Google login
+router.post("/google-login", (req, res) => {
+  const { idToken } = req.body;
+  const client = new OAuth2Client(constant.GOOGLE_CLIENT);
+  client
+    .verifyIdToken({ idToken, audience: constant.GOOGLE_CLIENT })
+    .then((resGG) => {
+      const { email_verified, name, email, picture } = resGG.payload;
+      if (email_verified) {
+        //Check if this email is existed
+        userModel
+          .getByUserEmail(email)
+          .then((data) => {
+            if (data) {
+              const access_token = jwt.sign(
+                commonUtils.parse2Plain(data),
+                constant.SECRET_KEY,
+                { expiresIn: 3600 }
+              );
+              const refresh_token = data.refresh_token;
+              data.refresh_token = undefined;
+              res.json({
+                access_token: access_token,
+                refresh_token: refresh_token,
+                user: data,
+              });
+            } else {
+              // This email not existed
+              // Register account with email from google
+              const rfToken = randomstring.generate({ length: 255 });
+              let userInfo = {
+                usergroupid: 2,
+                fullname: name,
+                email,
+                picture,
+                refresh_token: rfToken,
+                username: email,
+                password: "",
+              };
+              userModel
+                .addUserFromGG(userInfo)
+                .then((result) => {
+                  const access_token = jwt.sign(
+                    commonUtils.parse2Plain(userInfo),
+                    constant.SECRET_KEY
+                  );
+                  res.json({
+                    access_token,
+                    refresh_token: rfToken,
+                    user: omit(userInfo, ["refresh_token", "password"]),
+                  });
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } else {
+        res.status(401).json({
+          error: "Google login failed. Try again",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
     });
 });
 
+/**
+ * @api {post} /api/users/refresh-token Refresh Token
+ * @apiName Refresh Token
+ * @apiGroup Users
+ *
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *         "refresh_token": "QhnibXASPJRiDt46hDBWQGljURlZnfgkTOV8U6FRCkVqo0P3Rwx4QBjl9DBFyqLYt41jEiKQWjhUCTFn0ESFu2HLGgiu8pAXolHvhCGihEdCBKne3rh4erT5vv3Kc3yjM4EvjQ4Czine7D15oKT5Qh4d5uKgTrHao3BlzceL7HuTL9WKcWMn9YR9OVDoyjGsRwvNXKqE85xaT4XMWtoIdyH2vGCWTiVVF5T4cV2N2Ju4l7bVNrgD5CyXBd4gUFl"
+ *     }
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *         "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxNCIsInVzZXJuYW1lIjoidGllbnFkIiwiZnVsbG5hbWUiOiJRdWFjaCBEaW5oIFRpZW4iLCJwaG9uZU51bWJlciI6bnVsbCwiZW1haWwiOm51bGwsImdyb3VwQ29kZSI6IlNUVURFTlQiLCJpYXQiOjE2MDk1MTYxNzV9.zLGJpdGPrGs6BbfL5r6G1OW3xVhrG-cdzZEeczx2hAI"
+ *     }
+ */
+router.post(
+  "/refresh-token",
+  // validation(refreshTokenSchema),
+  (req, res, next) => {
+    let { access_token, refresh_token } = req.body;
+    const { userId } = jwt.verify(access_token, constant.SECRET_KEY, {
+      ignoreExpiration: true,
+    });
+    userModel
+      .findByIdAndRefreshToken(userId, refresh_token)
+      .then((data) => {
+        if (data) {
+          const access_token = jwt.sign(
+            commonUtils.parse2Plain(data),
+            constant.SECRET_KEY,
+            { expiresIn: 3600 }
+          );
+          res.json({
+            access_token: access_token,
+          });
+        } else {
+          throw "Refresh token fail";
+        }
+      })
+      .catch(next);
+  }
+);
+
+router.post("/check-login", (req, res) => {
+  try {
+    const access_token = req.headers.authorization;
+    const user = jwt.verify(access_token, constant.SECRET_KEY, {
+      ignoreExpiration: true,
+    });
+    if (!user)
+      return res.json({
+        isLogin: false,
+        message: "Can not find user",
+      });
+    userModel.getByUserEmail(user.email).then((data) => {
+      if (data) {
+        res.json({
+          isLogin: true,
+          user: omit(data, ["password", "refresh_token"]),
+        });
+      } else {
+        res.json({
+          isLogin: false,
+        });
+      }
+    });
+  } catch (error) {
+    throw error;
+  }
+});
+
+//Check existed email
+router.post("/check-email", async (req, res) => {
+  try {
+    const { email } = req.body;
+    //Check is existed user
+    const user = await userModel.findUserByEmail(email);
+    if (user) {
+      return res.json({
+        status: false,
+        message: "Email đã tồn tại, hãy đăng nhập!",
+      });
+    }
+    const otp = commonUtils.padLeadingZero(
+      rn({ min: 0000, max: 9999, integer: true }),
+      4
+    );
+    //check if user existed in OTP table
+    const userOTP = await userModel.findUserOTP(email);
+    if (!userOTP) {
+      await userModel.addOTP(email, otp);
+    } else {
+      await userModel.updateOTP(email, otp);
+    }
+    const dataToSend = {
+      to: email,
+      subject: "SuperSchool - Mã OTP",
+      html: `<div>Mã OTP của bạn là: ${otp}</div>`,
+    };
+    const sendMail = await mailService.sendOTPRegister(dataToSend);
+    if (sendMail) res.json({ status: true });
+  } catch (error) {
+    throw error;
+  }
+});
+//Confirm OTP
+router.post("/confirm-otp", async (req, res) => {
+  try {
+    const { email, otp } = req.body;
+    //check otp
+    const data = await userModel.checkOTP(email, otp);
+    if (data) return res.json({ status: true });
+    res.json({ status: false });
+  } catch (error) {
+    throw error;
+  }
+});
 module.exports = router;
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,153 +1,154 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const refreshTokenSchema = require('../schemas/refresh-token.json');
-const userModel = require('../models/user.model');
-const validation = require('../middlewares/validate.mdw');
-const roleValidation = require('../middlewares/validation.role');
-const commonUtils = require('../utils/common');
-const constant = require('../utils/constant');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const randomstring = require('randomstring');
-const db = require('../utils/db');
-const { transaction } = require('../utils/db');
-const validateMdw = require('../middlewares/validate.mdw');
+const userModel = require("../models/user.model");
+const validation = require("../middlewares/validate.mdw");
+const roleValidation = require("../middlewares/validation.role");
+const commonUtils = require("../utils/common");
+const constant = require("../utils/constant");
+const bcrypt = require("bcrypt");
+const randomstring = require("randomstring");
+const db = require("../utils/db");
 
-/**
- * @api {post} /api/users/refresh-token Refresh Token
- * @apiName Refresh Token
- * @apiGroup Users
- *
- * @apiParamExample {json} Request-Example:
- *     {
- *         "refresh_token": "QhnibXASPJRiDt46hDBWQGljURlZnfgkTOV8U6FRCkVqo0P3Rwx4QBjl9DBFyqLYt41jEiKQWjhUCTFn0ESFu2HLGgiu8pAXolHvhCGihEdCBKne3rh4erT5vv3Kc3yjM4EvjQ4Czine7D15oKT5Qh4d5uKgTrHao3BlzceL7HuTL9WKcWMn9YR9OVDoyjGsRwvNXKqE85xaT4XMWtoIdyH2vGCWTiVVF5T4cV2N2Ju4l7bVNrgD5CyXBd4gUFl"
- *     }
- *
- * @apiSuccessExample {json} Success-Response:
- *     HTTP/1.1 200 OK
- *     {
- *         "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxNCIsInVzZXJuYW1lIjoidGllbnFkIiwiZnVsbG5hbWUiOiJRdWFjaCBEaW5oIFRpZW4iLCJwaG9uZU51bWJlciI6bnVsbCwiZW1haWwiOm51bGwsImdyb3VwQ29kZSI6IlNUVURFTlQiLCJpYXQiOjE2MDk1MTYxNzV9.zLGJpdGPrGs6BbfL5r6G1OW3xVhrG-cdzZEeczx2hAI"
- *     }
- */
-router.post('/refresh-token', validation(refreshTokenSchema), (req, res, next) => {
-  let requestBody = req.body;
+router.post(
+  "/updateInfo",
+  validation(require("../schemas/updateInfo.json")),
+  (req, res, next) => {
+    let user = req.body;
 
-  userModel.findByIdAndRefreshToken(commonUtils.currentUser.userId, requestBody.refresh_token).then(data => {
-    if(data) {
-      const access_token = jwt.sign(commonUtils.parse2Plain(data), constant.SECRET_KEY);
-
-      res.json({
-        access_token: access_token
-      });
-    } else {
-      throw "Refresh token fail";
-    }
-  }).catch(next);
-});
-
-router.post('/updateInfo', validation(require('../schemas/updateInfo.json')), (req, res, next) => {
-  let user = req.body;
-  
-  db.transaction(transaction => {
-    if(user.userId != commonUtils.currentUser.userId) {
-      res.status(403).json({
-        message: 'You are not have permission to access this function'
-      });
-    }
-
-    userModel.updateInfo(user, commonUtils.currentUser).then(result => {
-      res.json({
-        message: 'Success'
-      });
-      transaction.commit();
-    }).catch(err => {
-      next(err);
-      transaction.rollback();
-    });
-  });
-});
-
-router.post('/changePassword', validation(require('../schemas/changePassword.json')), (req, res, next) => {
-  let user = req.body;
-
-  db.transaction(transaction => {
-    if (true) {}
-    userModel.findById(commonUtils.currentUser.userId).then(userFromDB => {
-      let encryptedPassword = userFromDB.password;
-  
-      //kiểm tra nhập đúng password hay không
-      let isValid = bcrypt.compareSync(user.oldPassword, encryptedPassword);
-      if(!isValid) {
-        res.status(500).json({
-          message: 'Password incorrect'
+    db.transaction((transaction) => {
+      if (user.userId != commonUtils.currentUser.userId) {
+        res.status(403).json({
+          message: "You are not have permission to access this function",
         });
-      } else {
-        if(user.oldPassword == user.newPassword) {
-          return res.status(500).json({
-            message: 'New password must be different from old password'
+      }
+
+      userModel
+        .updateInfo(user, commonUtils.currentUser)
+        .then((result) => {
+          res.json({
+            message: "Success",
           });
+          transaction.commit();
+        })
+        .catch((err) => {
+          next(err);
+          transaction.rollback();
+        });
+    });
+  }
+);
+
+router.post(
+  "/changePassword",
+  validation(require("../schemas/changePassword.json")),
+  (req, res, next) => {
+    let user = req.body;
+
+    db.transaction((transaction) => {
+      if (true) {
+      }
+      userModel.findById(commonUtils.currentUser.userId).then((userFromDB) => {
+        let encryptedPassword = userFromDB.password;
+
+        //kiểm tra nhập đúng password hay không
+        let isValid = bcrypt.compareSync(user.oldPassword, encryptedPassword);
+        if (!isValid) {
+          res.status(500).json({
+            message: "Password incorrect",
+          });
+        } else {
+          if (user.oldPassword == user.newPassword) {
+            return res.status(500).json({
+              message: "New password must be different from old password",
+            });
+          }
+
+          user.userId = commonUtils.currentUser.userId;
+          user.password = bcrypt.hashSync(
+            user.newPassword,
+            constant.SALT_ROUNDS
+          );
+          user.refresh_token = randomstring.generate({ length: 255 });
+          userModel
+            .changePassword(user)
+            .then((_) => {
+              transaction.commit();
+              res.json({
+                message: "Success",
+              });
+            })
+            .catch((err) => {
+              transaction.rollback();
+              next(err);
+            });
         }
-  
-        user.userId = commonUtils.currentUser.userId;
-        user.password = bcrypt.hashSync(user.newPassword, constant.SALT_ROUNDS);
+      });
+    });
+  }
+);
+
+router.get(
+  "/search",
+  roleValidation([constant.USER_GROUP.ADMIN]),
+  function (req, res, next) {
+    userModel
+      .search(req.body)
+      .then((users) => {
+        res.json({
+          data: users,
+        });
+      })
+      .catch(next);
+  }
+);
+
+router.get("/getTeacherInfo/:id", (req, res, next) => {
+  userModel
+    .getTeacherInfo(req.params.id)
+    .then((teacher) => {
+      res.json({
+        data: teacher,
+      });
+    })
+    .catch(next);
+});
+
+router.post(
+  "/create",
+  roleValidation([constant.USER_GROUP.ADMIN]),
+  validation(require("../schemas/createUpdateUser.json")),
+  (req, res, next) => {
+    db.transaction((transaction) => {
+      //init data before insert
+      let user = {};
+      let requestBody = req.body;
+
+      if (requestBody) {
+        user.username = requestBody.username || "";
+        user.password =
+          bcrypt.hashSync(requestBody.password, constant.SALT_ROUNDS) || "";
+        user.fullname = requestBody.fullname || "";
+        user.phonenumber = requestBody.phonenumber || "";
+        user.email = requestBody.email || "";
         user.refresh_token = randomstring.generate({ length: 255 });
-        userModel.changePassword(user).then(_ => {
+        user.usergroupid = requestBody.userGroupId;
+      }
+
+      userModel
+        .create(transaction, user)
+        .then((_) => {
           transaction.commit();
           res.json({
-            message: 'Success'
+            data: "Success",
           });
-        }).catch(err => {
+        })
+        .catch((err) => {
           transaction.rollback();
           next(err);
         });
-      }
-    })
-  });  
-})
-
-router.get('/search', roleValidation([constant.USER_GROUP.ADMIN]), function(req, res, next) {
-  userModel.search(req.body).then(users => {
-    res.json({
-      data: users
-    })
-  }).catch(next);
-});
-
-router.get('/getTeacherInfo/:id', (req, res, next) => {
-  userModel.getTeacherInfo(req.params.id).then(teacher => {
-    res.json({
-      data: teacher
-    })
-  }).catch(next);
-});
-
-router.post('/create', roleValidation([constant.USER_GROUP.ADMIN]), validation(require('../schemas/createUpdateUser.json')), (req, res, next) => {
-  db.transaction(transaction => {
-    //init data before insert
-    let user = {}
-    let requestBody = req.body;
-    
-    if(requestBody) {
-      user.username = requestBody.username || '';
-      user.password = bcrypt.hashSync(requestBody.password, constant.SALT_ROUNDS) || '';
-      user.fullname = requestBody.fullname || '';
-      user.phonenumber = requestBody.phonenumber || '';
-      user.email = requestBody.email || '';
-      user.refresh_token = randomstring.generate({ length: 255 });
-      user.usergroupid = requestBody.userGroupId;
-    }
-
-    userModel.create(transaction, user).then(_ => {
-      transaction.commit();
-      res.json({
-        data: 'Success'
-      });
-    }).catch(err => {
-      transaction.rollback();
-      next(err);
     });
-  });
-});
+  }
+);
 
 module.exports = router;
