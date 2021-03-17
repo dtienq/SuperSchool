f8c28be7a87741ae8c5794e163703e19cd98d760
Hoà Nguyễn <hoanht@outlook.com>
2021-03-17 23:53:17 +0700

    Fixing confliction after merge
    - Fixing merging confliction caused backend API down.

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
-const userModel = require('../models/user.model');
-const courseModel = require('../models/course.model');
-const constant = require('../utils/constant');
-const mailService = require('../utils/mailService');
-const bcrypt = require('bcrypt');
-const randomString = require('randomstring');
-var express = require('express');
-=======
 const userModel = require("../models/user.model");
+const courseModel = require("../models/course.model");
+const constant = require("../utils/constant");
+const mailService = require("../utils/mailService");
+const bcrypt = require("bcrypt");
 const randomString = require("randomstring");
-const categoryModel = require("../models/category.model");
 var express = require("express");
->>>>>>> 91c479188d68ca4cb5b55f98bfae6398bbefee50
+const categoryModel = require("../models/category.model");
 // const courseModel = require("../models/course.model");
 var router = express.Router();
 router.get("/listuser", (req, res, next) => {
@@ -53,23 +47,17 @@ router.post("/togglestatus", (req, res, next) => {
     .catch(next);
 });
 
-<<<<<<< HEAD
-router.post('/createteacher', (req, res, next) => {
+router.post("/createteacher", (req, res, next) => {
   const { fullname, email, picture, usergroupid } = req.body;
   const raw_password = randomString.generate({ length: 6 });
   const password = bcrypt.hashSync(raw_password, constant.SALT_ROUNDS);
-=======
-router.post("/createteacher", (req, res, next) => {
-  const { fullname, email, picture } = req.body;
-  const password = randomString.generate({ length: 6 });
->>>>>>> 91c479188d68ca4cb5b55f98bfae6398bbefee50
   const refresh_token = randomString.generate({ length: 255 });
   userModel
     .addAccount(fullname, email, usergroupid, picture, password, refresh_token)
     .then((data) => {
       const dataToSend = {
         to: email,
-        subject: 'SuperSchool - Thông tin đăng nhập',
+        subject: "SuperSchool - Thông tin đăng nhập",
         html: `<div>Email: ${email}</div>
                 <div>Mật khẩu:${raw_password}</div>`,
       };
