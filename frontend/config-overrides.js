9ad277299b9098691657aebd1c478c42770d98e3
PhanThanh07 <phanthanhvi97@gmail.com>
2021-03-23 12:33:50 +0700

    remove asset unused

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -8,7 +8,6 @@ module.exports = override(
     ['@hooks']: path.resolve(__dirname, 'src/hooks'),
     ['@utils']: path.resolve(__dirname, 'src/utils'),
     ['@api']: path.resolve(__dirname, 'src/api'),
-    ['@assets']: path.resolve(__dirname, 'src/assets'),
     ['@components']: path.resolve(__dirname, 'src/components'),
     ['@constants']: path.resolve(__dirname, 'src/constants'),
     ['@custom-fields']: path.resolve(__dirname, 'src/custom-fields'),
